0d4edb4c57af6581254831ac2abc3c2e2ab234e7
Jayanring <956165026@qq.com>
2023-06-20 16:23:26 +0800

    optim generate rand u64

--- src/storager.rs
+++ src/storager.rs
@@ -31,7 +31,6 @@ use opendal::{
 use prost::Message;
 use rand::Rng;
 use std::time::Duration;
-use tokio::task;
 
 #[derive(Clone)]
 pub struct Storager {
@@ -526,12 +525,10 @@ async fn backup(local: Storager, backup_interval_secs: u64, retreat_interval_sec
     let max_interval = backup_interval_secs * 4 / 3;
     'backup_round: loop {
         // sleep
-        let backup_interval = task::spawn_blocking(move || {
+        let backup_interval = {
             let mut rng = rand::thread_rng();
             rng.gen_range(min_interval..=max_interval)
-        })
-        .await
-        .unwrap();
+        };
         tokio::time::sleep(Duration::from_secs(backup_interval)).await;
 
         // avoid concurrent backup
